1adced23a68059e59cca46d4f9f1b563c78be18d
Никита Колташов <jenkinstix@gmail.com>
2022-01-27 18:43:27 +0500

    Initial commit

---
+++ src/pages/index/index.js
@@ -0,0 +1,15 @@
+/* import style */
+import './index.scss';
+/* import common js */
+import '../../js/common';
+/* import components and lib */
+import '../../js/components/demo';
+
+/* Your JS Code goes here */
+window.addEventListener('DOMContentLoaded', () => {
+
+});
+
+window.addEventListener('load', () => {
+
+});
