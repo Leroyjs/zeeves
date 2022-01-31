3625eee419c037854e0f30550b9aad4684548a4e
Никита Колташов <nikita@MacBook-Pro-Nikita.local>
2022-01-31 23:59:00 +0500

    first-slide height

--- src/js/common.js
+++ src/js/common.js
@@ -1,1 +1,11 @@
-window.addEventListener("DOMContentLoaded", () => {});
+window.addEventListener("DOMContentLoaded", () => {
+  let vh = window.innerHeight * 0.01;
+
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+  document.documentElement.style.setProperty("--fsvh", `${vh}px`);
+
+  window.addEventListener("resize", () => {
+    vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  });
+});
