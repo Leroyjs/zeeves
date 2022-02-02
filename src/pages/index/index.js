af9231f656328b5cbd4a48f68ba2cf94638c27db
Никита Колташов <nikita@MacBook-Pro-Nikita.local>
2022-02-03 00:11:34 +0500

    4k adaptive na polovinu(half) site

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -5,9 +5,7 @@ import "../../js/common";
 import { appearance } from "../../js/components/appearance";
 /* import partials and lib */
 import initHeader from '../../js/components/header';
-import { preloader } from '../../js/components/preloader';
 
-preloader()
 /* Your JS Code goes here */
 window.addEventListener('DOMContentLoaded', () => {
   initHeader();
