ce94da62f8b362c7ab56c02b446d9d27e9bb6b7e
Никита Колташов <nikita@MacBook-Pro-Nikita.local>
2022-07-19 10:28:33 +0500

    bug fix animation

--- src/js/components/gearRotation.js
+++ src/js/components/gearRotation.js
@@ -1,7 +1,9 @@
 export const gearRotation = () => {
   const fsGear = document.querySelector(".card-screen__gear");
+  const lsGear = document.querySelector(".bank-card-bg-bottom");
 
   window.addEventListener("scroll", () => {
     fsGear.style.transform = `rotate(${window.pageYOffset / 100}deg)`;
+    lsGear.style.transform = `rotate(${-window.pageYOffset / 50}deg)`;
   });
 };
